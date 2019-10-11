ddf7dfc2f5995b74f49f6ec95fe7b37fc35de806
Ronaldo Aragão <ronaldoarg@gmail.com>
2019-10-11 11:56:12 -0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-(function() {
+(function () {
 
-	const app = (function() {
+	const app = (function () {
 
 		var DOM = {};
 
@@ -15,43 +15,42 @@
 		function getCountWithoutSpace() {
 			return DOM.text.value.replace(/\s+/g, '').length;
 		}
-		
+
 		function getLines() {
 			return DOM.text.value.split('\n').length;
 		}
 
 		function getCountSpecificWord(word) {
-			return DOM.text.value.replace(/\n+/g, ' ').split(' ').filter((item) => {
-				return item === word && item != "";
-			}).length;
-	 	}
+			return DOM.text.value.replace(/\n+/g, ' ').split(' ').filter((item) => item === word && item != "").length;
+		}
 
 		function setCount() {
-			setValue(DOM.counter, getCount());			
-			setValue(DOM.counterWithoutSpace, getCountWithoutSpace());			
-			setValue(DOM.counterSpace, getCount() - getCountWithoutSpace());	
-			setValue(DOM.counterSpecificWord, +getCountSpecificWord(DOM.specificWord.value.replace(/\s+/g, '')));	
-			setValue(DOM.counterLines, getLines());	
+			setValue(DOM.counter, getCount());
+			setValue(DOM.counterWithoutSpace, getCountWithoutSpace());
+			setValue(DOM.counterSpace, getCount() - getCountWithoutSpace());
+			setValue(DOM.counterSpecificWord, +getCountSpecificWord(DOM.specificWord.value.replace(/\s+/g, '')));
+			setValue(DOM.counterLines, getLines());
 			animeResult();
 		}
 
 		function animeResult() {
-			if(!DOM.results.classList.contains('slideInDown')){
+			if (!DOM.results.classList.contains('slideInDown')) {
 				DOM.results.classList.add('slideInDown');
 			}
 		}
 
 		function init() {
 			DOM = {
-				text:                document.querySelector('[data-dom=text]'),
-				specificWord:        document.querySelector('[data-dom=specificWord]'),
-				counter:             document.querySelector('[data-dom=counter]'),
+				text: document.querySelector('[data-dom=text]'),
+				specificWord: document.querySelector('[data-dom=specificWord]'),
+				counter: document.querySelector('[data-dom=counter]'),
 				counterWithoutSpace: document.querySelector('[data-dom=counterWithoutSpace]'),
-				counterSpace:        document.querySelector('[data-dom=counterSpace]'),
-				counterLines:        document.querySelector('[data-dom=counterLines'),
+				counterSpace: document.querySelector('[data-dom=counterSpace]'),
+				counterLines: document.querySelector('[data-dom=counterLines'),
 				counterSpecificWord: document.querySelector('[data-dom=counterSpecificWord]'),
-				results:              document.querySelector('[data-dom=results]'),
+				results: document.querySelector('[data-dom=results]'),
 			}
+
 			DOM.text.addEventListener('input', setCount);
 			DOM.specificWord.addEventListener('input', setCount);
 		}
@@ -62,5 +61,5 @@
 	})();
 
 	app.init();
-    
+
 })();
